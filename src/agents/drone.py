e51cdaec3f53f0b7c21fe7027240762d010419f3
UShouldRun <henriqueteixeira845@gmail.com>
2025-10-23 12:33:37 +0100

    v0.5.0 - adding a star to drone

--- src/agents/drone.py
+++ src/agents/drone.py
@@ -5,21 +5,86 @@ from spade.behaviour import OneShotBehaviour, CyclicBehaviour
 from spade.message import Message
 from spade.template import Template
 
-from world.map import Map
+from world.map import Map, MapPos
 from world.world import World
+
+from heapq import heapify, heappush, heappop
 
 class Drone(Agent):
     class MapTerrain(CyclicBehaviour): # Is this a cyclic behaviour?
-        def a_star(self, map: Map, goal: Tuple[int, int]):
-            pass
+        class AStarNode():
+            def __init__(self, pos: MapPos, score: float) -> None:
+                self.pos   = pos
+                self.score = score
+
+            def __lt__(self, other) -> bool:
+                return self.score < other.score
+
+        def reconstruct(path: dict[MapPos, MapPos], start: MapPos, goal: MapPos) -> List[MapPos]:
+            node: MapPos = goal
+            seq: List[MapPos] = []
+            while node != start:
+                seq.insert(0, node)
+                node = path[node]
+            return seq
+
+        def a_star(self, map: Map, start: Tuple[float, float], goal: Tuple[float, float]) -> List[Tuple[float, float]]:
+            s: MapPos = map.normalize(start)
+            g: MapPos = map.normalize(goal)
+
+            min_heap: List[MapPos] = []
+            heapify(min_heap)
+
+            path: dict[MapPos, MapPos] = {}
+
+            gScore: dict[MapPos, float] = {
+                    (i, j): float("inf")
+                    for i in range(map.length)
+                    for j in range(map.height)
+                }
+            gScore[s] = 0
+            heappush(min_heap, AStarNode(s, gScore[s]))
+
+            fScore: dict[MapPos, float] = {
+                    (i, j): float("inf")
+                    for i in range(map.length)
+                    for j in range(map.height)
+                }
+            fScore[s] = map.distance(s, g)
+
+            while queue != []:
+                curr: MapPos = heappop(min_heap).pos
+                if curr[0] == g[0] and curr[1] == g[1]:
+                    return reconstruct(path, s, g)
+
+                neighbours: List[MapPos] = filter(
+                        lambda pos: map.in_map(pos) and ,
+                        [(curr[0] + dir_x, curr[1] + dir_y)
+                         for dir_x, dir_y in [
+                             (-1,-1), (0,-1), (1,-1), (1,0),
+                             (1,1), (0,1), (-1,1), (-1,0)
+                        ]]
+                    )
+                for neighbour in neighbours:
+                    tentative_gScore: float = gScore[curr] + map.distance(curr, neighbour)
+
+                    if tentative_gScore < gScore[neighbour]:
+                        path[neighbour]   = curr
+                        gScore[neighbour] = tentative_gScore
+                        fScore[neighbour] = tentative_gScore + map.distance(neighbour, g)
+
+                        if neighbour not in min_heap:
+                            heappush(min_heap, AStarNode(neighbour, gScore[neighbour]))
+
+            return []
 
         async def on_start(self, map: Map, world: World):
             """Initial Scan"""
             map.add(world.objects)
  
-        async def run(self, map: Map, goal: Tuple[int, int]):
+        async def run(self, map: Map, goal: MapPos) -> List[Tuple[float, float]]:
             """Moves to goal location"""
-           return self.a_star(map, goal)
+            return self.a_star(map, goal)
 
         async def on_end(self):
             """Stops to analyze"""
--- src/world/map.py
+++ src/world/map.py
@@ -1,18 +1,34 @@
+from math import sqrt
+
+MapPos = Tuple[int, int]
+
 class Map():
     def __init__(self, limit: Tuple[int, int]) -> None:
         self.graph:  int = 0
         self.length: int = limit[0]
         self.height: int = limit[1]
 
-    def normalize(self, pos: Tuple[float, float]) -> Tuple[int, int]:
+    def normalize(pos: Tuple[float, float]) -> Tuple[int, int]:
         return (int(pos[0] / self.height), int(pos[1] / self.length))
+
+    def distance(self, pos1: Tuple[float, float], pos2: Tuple[float, float]) -> float:
+        i1, j1 = self.normalize(pos1)
+        i2, j2 = self.normalize(pos2)
+        return sqrt((i1 - i2) ** 2 + (j1 - j2) ** 2)
+
+    def in_map(self, pos: Tuple[int, int]) -> bool:
+        return 0 <= pos[0] < self.length and 0 <= pos[1] < self.height
 
     def add(self, obstacles: List[WorldObject]) -> None:
         for obj in obstacles:
-            pos: Tuple[int, int] = self.normalize(obj.pos)
+            pos: Tuple[int, int] = Map.normalize(obj.pos)
             self.graph |= 1 << (pos[0] * self.length + pos[1])
 
     def remove(self, obstables: List[WorldObject]) -> None:
         for obj in obstacles:
-            pos: Tuple[int, int] = self.normalize(obj.pos)
+            pos: Tuple[int, int] = Map.normalize(obj.pos)
             self.graph &= (1 << (pos[0] * self.length + pos[1])) - 1
+
+    def pos_is_blocked(self, pos: Tuple[float, float]) -> bool:
+        i, j = Map.normalize(pos)
+        return (self.graph >> (i * self.length + j)) & 1
